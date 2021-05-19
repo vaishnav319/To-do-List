3e5f9d5ab2af8c14a1490093384fcd00c8f9f241
vaishnav319 <68817230+vaishnav319@users.noreply.github.com>
2021-05-19 11:44:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date=require(__dirname+"/date.js");
+
+const app = express();
+const items=["Buy Food","Eat Food","Cook food"];
+const workItems=[];
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"))
+
+
+app.get("/", function(req, res) {
+  const day=date.getDate();
+  res.render("list", {listTitle: day,newListItems:items,});
+});
+
+
+app.post("/",function(req,res){
+   const item=req.body.newItem;
+
+    if(item!=""){
+     if(req.body.list === "Work"){
+       workItems.push(item);
+       res.redirect("/work");
+     }else{
+       items.push(item);
+       res.redirect("/");
+     }
+   }
+
+
+
+})
+
+app.get("/work",function(req,res){
+  res.render("list",{listTitle:"Work List",newListItems:workItems});
+});
+
+app.get("/about",function(req,res){
+  res.render("about");
+})
+
+
+
+app.listen(3000, function() {
+  console.log("Hey ur server is running on port 3000");
+})
